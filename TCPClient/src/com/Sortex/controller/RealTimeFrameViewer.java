52a627d27594aa67714a37904a6ba9df216d4a0a
AselaDarshan <aseladarshan@gmail.com>
2017-11-15 17:16:07 +0530

    remove frame buffer

---
+++ TCPClient/src/com/Sortex/controller/RealTimeFrameViewer.java
@@ -0,0 +1,15 @@
+package com.Sortex.controller;
+
+public class RealTimeFrameViewer {
+	
+	public void startView() {
+		
+	}
+	
+	public void stopView() {
+		TCPClient.buildServerConnection();
+		
+		
+	}
+
+}
